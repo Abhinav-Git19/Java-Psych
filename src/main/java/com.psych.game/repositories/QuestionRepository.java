dba8e86375837fc675e3c7c620f207f503f47b5b
Abhinav Singh <abhinavsingh@Abhinavs-MacBook-Air.local>
2020-02-23 15:55:45 +0530

    Include Repository Annotations in QuestionsRepository

--- src/main/java/com.psych.game/repositories/QuestionRepository.java
+++ src/main/java/com.psych.game/repositories/QuestionRepository.java
@@ -2,6 +2,8 @@ package com.psych.game.repositories;
 
 import com.psych.game.models.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QuestionRepository extends JpaRepository<Question,Long> {
 }
